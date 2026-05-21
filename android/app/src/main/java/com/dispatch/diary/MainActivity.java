f4a1c06d3bd5ee7a0bec979fdf7a22d5e27ed5a5
Kiddow <theokgomza0306@gmail.com>
2026-05-21 21:10:51 +0200

    chore: setup capacitor and android github action

---
+++ android/app/src/main/java/com/dispatch/diary/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dispatch.diary;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dispatch.diary',
+  appName: 'DispatchDiary',
+  webDir: 'dist/client'
+};
+
+export default config;
